592db7792b76eba3cab80e7228a0eb25ceb84659
Junting <junting.lu@gmail.com>
2025-02-27 00:27:34 +0000

    built tcp server in node using only docs

---
+++ node-tcp-server/server.mjs
@@ -0,0 +1,33 @@
+import net from "net";
+
+const port = 1337;
+const host = "127.0.0.1";
+
+const server = new net.createServer((socket) => {
+  socket.on("data", (data) => {
+    console.log(`Date received: ${data}`);
+    // console.log(typeof data);
+    // socket.write(data);
+    // socket.pipe(socket);
+  });
+});
+
+server.on("error", (err) => {
+  throw new Error(err);
+});
+
+server.on("connection", (socket) => {
+  console.log(
+    `Server: ${socket.remoteAddress}:${socket.remotePort} has connected`,
+  );
+});
+
+server.on("listening", () => {
+  const address = server.address();
+  console.log(`opened server on ${address.address}:${address.port}`);
+});
+
+server.listen(port, host);
+
+// Use netcat on OSX to test in terminal
+// nc 127.0.0.1 1337
