dd7d5c762e92e58702e477e0977383edc46c29b2
Junting <junting.lu@gmail.com>
2025-02-27 01:57:53 +0000

    update server to track connected clients and broadcast msgs to other clients

--- node-tcp-server/server.mjs
+++ node-tcp-server/server.mjs
@@ -3,23 +3,30 @@ import net from "net";
 const port = 1337;
 const host = "127.0.0.1";
 
-const server = new net.createServer((socket) => {
-  socket.on("data", (data) => {
-    console.log(`Data received: ${data}`);
-    // console.log(typeof data);
-    socket.write(data.toString().toUpperCase());
-    // socket.pipe(socket);
-  });
+const connectedClients = [];
+
+const server = new net.createServer((clientSocket) => {
+  // moved to connection listener
 });
 
 server.on("error", (err) => {
   throw new Error(err);
 });
 
-server.on("connection", (socket) => {
+server.on("connection", (clientSocket) => {
   console.log(
-    `Server: ${socket.remoteAddress}:${socket.remotePort} has connected`,
+    `Server: ${clientSocket.remoteAddress}:${clientSocket.remotePort} has connected`,
   );
+
+  clientSocket.on("data", (data) => {
+    connectedClients.forEach((cc) => {
+      if (clientSocket != cc) cc.write(data.toString().toUpperCase());
+    });
+    // console.log(typeof data);
+    // clientSocket.pipe(clientSocket);
+  });
+
+  connectedClients.push(clientSocket);
 });
 
 server.on("listening", () => {
